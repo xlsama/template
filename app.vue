6eb58f8a1d4246088feffb32253da0149a05f4f7
xlsama <im.xlsama@gmail.com>
2025-06-26 14:19:28 +0800

    chore: add tailwindcss

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+	<div>
+    <h1>hello</h1>
+    <h1 class="text-yellow-400 text-lg"">world</h1>
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/image']
-})
+	compatibilityDate: "2025-05-15",
+	devtools: { enabled: true },
+	modules: ["@nuxt/fonts", "@nuxt/image"],
+	css: ["~/assets/css/main.css"],
+	vite: {
+		plugins: [tailwindcss() as any],
+	},
+});
