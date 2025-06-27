1d2cca14e5cc6a04e829bbcb88685bca04f0dfc0
xlsama <im.xlsama@gmail.com>
2025-06-27 11:05:36 +0800

    chore: update

--- app.vue
+++ app.vue
@@ -1,16 +1,14 @@
 <template>
-	<div class="p-10">
-		<h1>hello</h1>
-		<h1 class="text-yellow-400 text-lg">world</h1>
-		<div class="flex gap-2">
-			<Button label="Primary" />
-			<Button label="Success" severity="success" />
-			<Button icon="pi pi-home" aria-label="Save" />
+  <div class="p-10">
+    <div class="flex gap-2">
+      <Button label="Primary" />
+      <Button label="Success" severity="success" />
+      <Button aria-label="Save" icon="pi pi-home" />
 
-			<Button label="Danger" severity="danger" />
-			<Button type="button" label="Search" icon="pi pi-search" />
-		</div>
-	</div>
-	
-	<NuxtPage/>
+      <Button label="Danger" severity="danger" />
+      <Button icon="pi pi-search" label="Search" type="button" />
+    </div>
+  </div>
+
+  <NuxtPage />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-
+const inputValue = ref('')
 </script>
 
 <template>
-<div>home page</div>
+  <div class="px-10">
+    <InputText type="text" v-model="inputValue" />
+    
+    <Panel header="Input Value:" class="mt-4">
+    <p class="m-0">
+{{ inputValue }}
+    </p>
+</Panel>
+
+  </div>
+
+
 </template>
